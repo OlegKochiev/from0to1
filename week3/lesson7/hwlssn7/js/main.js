03a60a76fd7c2919aceb7cf15003db7d41fc7ef6
Oleg Kochiev <developer-artisan@mail.ru>
2021-11-21 20:03:21 +0300

    was added changes and finished hwlssn7

--- week3/lesson7/hwlssn7/js/main.js
+++ week3/lesson7/hwlssn7/js/main.js
@@ -1,8 +1,22 @@
 $(function () {
-	var mixer = mixitup('.works__list');
 	$('.comments__list').slick({
 		dots: true,
 		prevArrow: false,
 		nextArrow: false
 	})
+
+	$('.header__btn').on('click', function () {
+		$('.header__list').toggleClass('header__list--active');
+		$('.burger__btn-line').toggleClass('burger__btn-line--active');
+		$('body').toggleClass('hidden');
+	});
+
+	$('.header__link').on('click', function () {
+		$('.header__list').toggleClass('header__list--active');
+		$('.burger__btn-line').toggleClass('burger__btn-line--active');
+		$('body').toggleClass('hidden');
+	});
+
+
+	var mixer = mixitup('.works__list');
 })
--- week3/lesson7/js/main.js
+++ week3/lesson7/js/main.js
@@ -1,8 +1,14 @@
 $(function () {
-	var mixer = mixitup('.portfolio__content');
+
 	$('.slider-blog__inner').slick({
 		dots: true,
 		prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow_left.svg" alt="Стрелка влево"></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="images/arrow_right.svg" alt="Стрелка вправо"></button>'
 	})
+
+	$('.menu__btn').on('click', function () {
+		$('.header__top-inner').toggleClass('header__top-inner--active');
+	});
+
+	var mixer = mixitup('.portfolio__content');
 });
